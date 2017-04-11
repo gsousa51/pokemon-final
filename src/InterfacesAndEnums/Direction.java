7cb866af5dba2e2d0775807350cbaf9e9ebc7801
bhudnell1240 <bthudnell@gmail.com>
2017-04-11 15:50:20 -0700

    Added the main interface between the GUI and the model

---
+++ src/InterfacesAndEnums/Direction.java
@@ -0,0 +1,12 @@
+package InterfacesAndEnums;
+
+/**
+ * An enum used to represent one of four directions in which the player 
+ * will move or shoot an arrow.
+ *
+ * @author Brendon Hudnell
+ */
+public enum Direction {
+  NORTH, EAST, SOUTH, WEST;
+}
+ 
---
+++ src/InterfacesAndEnums/GameInterface.java
@@ -0,0 +1,8 @@
+package InterfacesAndEnums;
+import java.awt.Point;
+
+public interface GameInterface {
+	public MapObject[][] getMap();
+	
+	public Point getPlayerPosition();
+}
