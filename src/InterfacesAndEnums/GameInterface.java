369c98f82de99b991317d01e53a09cf1cef89296
morganjhenry <morganhenry@email.airzona.edu>
2017-04-12 15:19:13 -0700

    Fixed spelling and format in GameInterface

--- src/InterfacesAndEnums/GameInterface.java
+++ src/InterfacesAndEnums/GameInterface.java
@@ -1,9 +1,9 @@
 import java.awt.Point;
-  
-    public interface GameInterface {
-        public MapObject[][] getMap();
-           public Point getPlayerPosition();
-               public Point getTrainerPosition();
-               public int getRemaingSteps();
-               public void moveTrainer(Direction dir);
-               }
+
+public interface GameInterface
+{
+    public MapObject[][] getMap();
+    public Point getTrainerPosition();
+    public int getRemainingSteps();
+    public void moveTrainer(Direction dir);
+}
