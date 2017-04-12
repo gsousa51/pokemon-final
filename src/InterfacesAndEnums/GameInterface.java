51050be9ea2e2fdea9d0cd397d8006af0c7544b4
morganjhenry <morganhenry@email.airzona.edu>
2017-04-12 15:06:18 -0700

    Updated GameInterface

--- src/InterfacesAndEnums/GameInterface.java
+++ src/InterfacesAndEnums/GameInterface.java
@@ -2,7 +2,9 @@ package InterfacesAndEnums;
 import java.awt.Point;
 
 public interface GameInterface {
+
 	public MapObject[][] getMap();
-	
-	public Point getPlayerPosition();
+	public Point getTrainerPosition();
+	public int getRemaingSteps();
+	public void moveTrainer(Direction dir);
 }
