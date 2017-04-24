9152b39ae14bcff76ad1913316e40421fcacf2da
bhudnell1240 <bthudnell@gmail.com>
2017-04-24 00:25:48 -0700

    added ballsLeft to game

--- src/model/Game.java
+++ src/model/Game.java
@@ -12,102 +12,90 @@ import interfaceEnumMocks.MapObject;
 // import InterfacesAndEnums.GameInterface;
 // import InterfacesAndEnums.MapObject;
 
-public class Game implements GameInterface, Serializable
-{
-    private  Map gameMap;
-    private  Trainer trainer;
-    private MyPokemonList pokedex;
-    private GameOverOptions endGame;
-    
+public class Game implements GameInterface, Serializable {
+	private Map gameMap;
+	private Trainer trainer;
+	private MyPokemonList pokedex;
+	private GameOverOptions endGame;
 
-    public Game(int map, GameOverOptions end){
-    	gameMap = new Map(map);
-    	trainer = new Trainer();
-    	pokedex = MyPokemonList.getInstance();
-    	endGame = end;
-    }
-    
-    @Override
-    public MapObject[][] getMap()
-    {
-        return gameMap.getMap();
-    }
-    
-    public int getMapWidth()
-    {
-        return gameMap.getMapWidth();
-    }
-    
-    public int getMapHeight()
-    {
-        return gameMap.getMapHeight();
-    }
-    
-    public void addPokemon(Pokemon pok)
-    {
-        trainer.addPokemon(pok);
-    }
-    
-    @Override
-    public Point getTrainerPosition()
-    {
-        return trainer.getPosition();
-    }
+	public Game(int map, GameOverOptions end) {
+		gameMap = new Map(map);
+		trainer = new Trainer();
+		pokedex = MyPokemonList.getInstance();
+		endGame = end;
+	}
 
-    @Override
-    public int getRemainingSteps()
-    {
-        return trainer.getRemainingStep();
-    }
-    
-    public void throwBall()
-    {
-        trainer.throwBall();
-    }
-    
-    public void findBall()
-    {
-        trainer.findBall();
-    }
+	@Override
+	public MapObject[][] getMap() {
+		return gameMap.getMap();
+	}
 
-    @Override
-    public void moveTrainer(Direction dir)
-    {
-        trainer.move(dir);
-    }
-    
-    public Pokemon checkPokemon()
-    {
-        if(Math.random() < .5)
-            return AvailablePokemonList.getInstance().getPokemon();
-        else
-            return null;
-    }
-    
-    /**
-     * This method will return true if the game is over based
-     * on the users end game choice
-     * @return
-     * 
-     * @author Morgan Henry
-     */
-    public boolean gameOver()
-    {
-        if(getRemainingSteps() <= 0 && endGame == GameOverOptions.NO_STEPS)
-            return true;
-        else if(trainer.getRemainingBalls() <= 0 && endGame == GameOverOptions.NO_BALL)
-            return true;
-        else if(MyPokemonList.getInstance().size() == 15 & endGame == GameOverOptions.POKEMON_CAUGHT)
-            return true;
-        else
-            return false;    
-    }
-    
-    //TODO: Implement ballsLeft()
-//    public int ballsLeft(){
-//    	//TODO: implementThis
-//    }
-    public int getPokemonCount(){
-    	return pokedex.size();
-    }
+	public int getMapWidth() {
+		return gameMap.getMapWidth();
+	}
+
+	public int getMapHeight() {
+		return gameMap.getMapHeight();
+	}
+
+	public void addPokemon(Pokemon pok) {
+		trainer.addPokemon(pok);
+	}
+
+	@Override
+	public Point getTrainerPosition() {
+		return trainer.getPosition();
+	}
+
+	@Override
+	public int getRemainingSteps() {
+		return trainer.getRemainingStep();
+	}
+
+	public void throwBall() {
+		trainer.throwBall();
+	}
+
+	public void findBall() {
+		trainer.findBall();
+	}
+
+	@Override
+	public void moveTrainer(Direction dir) {
+		trainer.move(dir);
+	}
+
+	public Pokemon checkPokemon() {
+		if (Math.random() < .5)
+			return AvailablePokemonList.getInstance().getPokemon();
+		else
+			return null;
+	}
+
+	/**
+	 * This method will return true if the game is over based on the users end
+	 * game choice
+	 * 
+	 * @return
+	 * 
+	 * @author Morgan Henry
+	 */
+	public boolean gameOver() {
+		if (getRemainingSteps() <= 0 && endGame == GameOverOptions.NO_STEPS)
+			return true;
+		else if (trainer.getRemainingBalls() <= 0 && endGame == GameOverOptions.NO_BALL)
+			return true;
+		else if (MyPokemonList.getInstance().size() == 15 & endGame == GameOverOptions.POKEMON_CAUGHT)
+			return true;
+		else
+			return false;
+	}
+
+	public int ballsLeft() {
+		return trainer.getRemainingBalls();
+	}
+
+	public int getPokemonCount() {
+		return pokedex.size();
+	}
 }
