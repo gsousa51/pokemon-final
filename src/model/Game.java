29816465579fe1836804345d80c6989203a05e17
morganjhenry <morganhenry@email.airzona.edu>
2017-04-26 10:56:25 -0700

    Added to game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -14,40 +14,40 @@ import interfaceEnumMocks.MapObject;
 
 public class Game implements GameInterface, Serializable
 {
-    private  Map gameMap;
-    private  Trainer trainer;
+    private Map gameMap;
+    private Trainer trainer;
     private MyPokemonList pokedex;
     private GameOverOptions endGame;
-    
 
-    public Game(int map, GameOverOptions end){
-    	gameMap = new Map(map);
-    	trainer = new Trainer();
-    	pokedex = MyPokemonList.getInstance();
-    	endGame = end;
+    public Game(int map, GameOverOptions end)
+    {
+        gameMap = new Map(map);
+        trainer = new Trainer();
+        pokedex = MyPokemonList.getInstance();
+        endGame = end;
     }
-    
+
     @Override
     public MapObject[][] getMap()
     {
         return gameMap.getMap();
     }
-    
+
     public int getMapWidth()
     {
         return gameMap.getMapWidth();
     }
-    
+
     public int getMapHeight()
     {
         return gameMap.getMapHeight();
     }
-    
+
     public void addPokemon(Pokemon pok)
     {
         trainer.addPokemon(pok);
     }
-    
+
     @Override
     public Point getTrainerPosition()
     {
@@ -59,12 +59,12 @@ public class Game implements GameInterface, Serializable
     {
         return trainer.getRemainingStep();
     }
-    
+
     public void throwBall()
     {
         trainer.throwBall();
     }
-    
+
     public void findBall()
     {
         trainer.findBall();
@@ -75,37 +75,37 @@ public class Game implements GameInterface, Serializable
     {
         trainer.move(dir);
     }
-    
+
     public Pokemon checkPokemon()
     {
-        if(Math.random() < .5)
+        if (Math.random() < .5)
             return AvailablePokemonList.getInstance().getPokemon();
         else
             return null;
     }
-    
+
     /**
-     * This method will return true if the game is over based
-     * on the users end game choice
+     * This method will return true if the game is over based on the users end
+     * game choice
+     * 
      * @return
      * 
      * @author Morgan Henry
      */
     public boolean gameOver()
     {
-        if(getRemainingSteps() <= 0 && endGame == GameOverOptions.NO_STEPS)
+        if (getRemainingSteps() <= 0 && endGame == GameOverOptions.NO_STEPS)
             return true;
-        else if(trainer.getRemainingBalls() <= 0 && endGame == GameOverOptions.NO_BALL)
+        else if (trainer.getRemainingBalls() <= 0 && endGame == GameOverOptions.NO_BALL)
             return true;
-        else if(MyPokemonList.getInstance().size() == 15 & endGame == GameOverOptions.POKEMON_CAUGHT)
+        else if (MyPokemonList.getInstance().size() == 15 & endGame == GameOverOptions.POKEMON_CAUGHT)
             return true;
         else
-            return false;    
+            return false;
     }
-    
-    
-    
-    public int getPokemonCount(){
-    	return pokedex.size();
+
+    public int getPokemonCount()
+    {
+        return pokedex.size();
     }
 }
