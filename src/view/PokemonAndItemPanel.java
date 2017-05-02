c6a51ca57e32fb6020368d8efa84ac290aae16f4
Stephen Nolan <spn@email.arizona.edu>
2017-05-02 09:43:19 -0700

    lock main UI when user views items and pokemon

--- src/view/PokemonAndItemPanel.java
+++ src/view/PokemonAndItemPanel.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -9,29 +12,50 @@ import model.Game;
 
 public class PokemonAndItemPanel extends JPanel {
 
-	// Instance variables
-	private static final long serialVersionUID = 1L;
-	private Game game;
-	private GameFrame container;
+    // Instance variables
+    private static final long serialVersionUID = 1L;
+    private Game game;
+    private GameFrame container;
+    private JFrame frame;
 
 
-	// Constructor
-	public PokemonAndItemPanel(Game game, GameFrame container) {
+    // Constructor
+    public PokemonAndItemPanel(Game game, GameFrame container) {
 
-		this.game = game;
-		this.container = container;
-		displayPanel();
-	}
+        this.game = game;
+        this.container = container;
+        displayPanel();
+    }
 
-	private void displayPanel() {
-        
-        JFrame frame = new JFrame();
+    private void displayPanel() {
 
-		frame.setSize(800, 800);
-		frame.setLayout(null);
+        // Lock the main UI
+        this.container.setEnabled(false);
+        System.out.println("Main UI locked.");
+
+        // Frame to hold the display
+        frame = new JFrame();
+        frame.setTitle("Pokemon and Items");
+        frame.setSize(800, 800);
+        frame.setLayout(null);
         frame.setVisible(true);
+        // Unlock the main UI when window closed
+        frame.addWindowListener(new ClosedPanelAdapter());
+
+        // Add item and pokemon views
         frame.add(this);
 
-		repaint();
-	}
+        repaint();
+    }
+
+    private class ClosedPanelAdapter extends WindowAdapter {
+
+        @Override
+        public void windowClosing(WindowEvent event) {
+
+            PokemonAndItemPanel.this.container.setEnabled(true);
+            System.out.println("Main UI unlocked.");
+
+        }
+    }
 }
